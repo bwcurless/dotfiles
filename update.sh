58869db8aa61dd84a355be1667ce015decaeb2f7
Brian Curless <bcurl3ss@gmail.com>
2023-08-15 12:49:19 -0700

    Adding basic dotfiles to repo

---
+++ update.sh
@@ -0,0 +1,6 @@
+#!usr/bin/env/ bash
+#Create symlinks for all dotfiles
+ln -sf dotfiles/.vimrc ~/.vimrc
+ln -sf dotfiles/.bashrc ~/.bashrc
+ln -sf dotfiles/.gitconfig ~/.gitconfig
+ln -sf dotfiles/.tmux.conf ~/.tmux.conf
