035db88157ae669b9bde043ffa0f398b822839d2
Brian Curless <bcurl3ss@gmail.com>
2023-08-17 21:58:50 -0700

    Added git ignore global

--- update.sh
+++ update.sh
@@ -4,3 +4,4 @@ ln -sf dotfiles/.vimrc ~/.vimrc
 ln -sf dotfiles/.bashrc ~/.bashrc
 ln -sf dotfiles/.gitconfig ~/.gitconfig
 ln -sf dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf dotfiles/.gitignore_global ~/.gitignore_global
