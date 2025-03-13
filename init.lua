ab7408a2bc65c1c7af83fc64f7c8ed7c76897579
Brian Curless <bcurl3ss@gmail.com>
2025-03-13 16:22:46 -0700

    Updated omnisharp config for speed.
    It was crazy slow before. Now, the startup time is slow, but atleast it
    runs ok. It also doesn't have tons of warnings everywhere.

--- init.lua
+++ init.lua
@@ -129,7 +129,7 @@ end, { desc = 'Telescope help tags' })
 -----------------------
 --- Treesitter
 ----------------------
----
+
 require 'nvim-treesitter.configs'.setup {
 	-- A list of parser names, or "all" (the listed parsers MUST always be installed)
 	ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "cuda", "cpp", "python", "c_sharp" },
@@ -266,6 +266,8 @@ require 'lspconfig'.bashls.setup {
 	handlers = handlers }
 require 'lspconfig'.clangd.setup {
 	handlers = handlers }
+require 'lspconfig'.jsonls.setup {
+	handlers = handlers }
 require 'lspconfig'.lua_ls.setup {
 	handlers = handlers,
 	capabilities = capabilities,
@@ -313,7 +315,7 @@ require 'lspconfig'.omnisharp.setup {
 		FormattingOptions = {
 			-- Enables support for reading code style, naming convention and analyzer
 			-- settings from .editorconfig.
-			EnableEditorConfigSupport = true,
+			EnableEditorConfigSupport = false,
 			-- Specifies whether 'using' directives should be grouped and sorted during
 			-- document formatting.
 			OrganizeImports = true,
@@ -325,21 +327,21 @@ require 'lspconfig'.omnisharp.setup {
 			-- for projects that are relevant to code that is being edited. With this
 			-- setting enabled OmniSharp may load fewer projects and may thus display
 			-- incomplete reference lists for symbols.
-			LoadProjectsOnDemand = nil,
+			LoadProjectsOnDemand = true,
 		},
 		RoslynExtensionsOptions = {
 			-- Enables support for roslyn analyzers, code fixes and rulesets.
-			EnableAnalyzersSupport = true,
+			EnableAnalyzersSupport = false,
 			-- Enables support for showing unimported types and unimported extension
 			-- methods in completion lists. When committed, the appropriate using
 			-- directive will be added at the top of the current file. This option can
 			-- have a negative impact on initial completion responsiveness,
 			-- particularly for the first few completion sessions after opening a
 			-- solution.
-			EnableImportCompletion = true,
+			EnableImportCompletion = false,
 			-- Only run analyzers against open files when 'enableRoslynAnalyzers' is
 			-- true
-			AnalyzeOpenDocumentsOnly = nil,
+			AnalyzeOpenDocumentsOnly = true,
 		},
 		Sdk = {
 			-- Specifies whether to include preview versions of the .NET SDK when
