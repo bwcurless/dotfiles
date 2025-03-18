afab9c4709c7f5cadf2f9b200d75b8370e9b9454
Brian Curless <bcurl3ss@gmail.com>
2025-03-17 17:11:44 -0700

    Added grep string under cursor map.

--- init.lua
+++ init.lua
@@ -300,6 +300,8 @@ vim.keymap.set('n', '<C-f>', builtin.find_files, { desc = 'Telescope find files'
 vim.keymap.set('n', '<C-g>', builtin.git_files, { desc = 'Telescope find git files' })
 vim.keymap.set('n', '<leader>b', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep working directory' })
+vim.keymap.set('n', '<leader>fc', builtin.grep_string,
+	{ desc = 'Telescope grep string under cursor in working directory' })
 vim.keymap.set("n", "<leader>fl", function()
 	require('telescope.builtin').live_grep({
 		prompt_title = "Search through plugins",
