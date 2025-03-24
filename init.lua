4d71bbbae60b9e2b27f36154de0e0d31096d78c6
Brian Curless <bcurl3ss@gmail.com>
2025-03-24 15:11:33 -0700

    Created key bindings for quickly launching terminal.

--- init.lua
+++ init.lua
@@ -180,6 +180,29 @@ vim.diagnostic.config({
 })
 
 -------------------
+--- Terminal setup
+-------------------
+
+--Configure terminal when it is opened
+vim.api.nvim_create_autocmd({ "TermOpen" }, {
+	group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
+	callback = function()
+		vim.opt.number = false
+		vim.opt.relativenumber = false
+		vim.api.nvim_cmd({ cmd = "startinsert" }, { output = false })
+	end
+})
+
+-- Start terminal shortcut
+vim.keymap.set('n', "<leader>st", function()
+	vim.cmd.vnew()
+	vim.cmd.term()
+	vim.cmd.wincmd("J")
+	vim.api.nvim_win_set_height(0, 10)
+end)
+
+
+-------------------
 --- Plugin setup
 -------------------
 
