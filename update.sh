d0b04c3e413be2f2ce951d45e4ea266735702c80
Brian Curless <bcurl3ss@gmail.com>
2025-04-03 18:26:29 -0700

    Reorganized neovim config to be in its own folder.
    Updated update.sh to handle symlinking entire neovim config folder over.
    This will allow me to better organize my init.lua which has gotten out
    of hand.

--- update.sh
+++ update.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/bash
-#Create symlinks for all dotfiles
+#Create symlinks for files
 # Symlink and force (delete file if it exists and replace with link)
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/init.lua ~/.config/nvim/init.lua
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
@@ -10,11 +8,14 @@ ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
 mkdir -p ~/.docker
 ln -sf ~/dotfiles/.docker/config.json ~/.docker/config.json
-mkdir -p ~/.vim/spell
-# Link all files here to this target directory
-ln -sf ~/dotfiles/spell/* ~/.vim/spell
-# Make a local folder in dotfiles (This doesn't seem very useful)
-mkdir -p ~/UltiSnips
+
+# Neovim setup
+# init.lua, lua/...
+ln -sf ~/dotfiles/neovim/* ~/.config/nvim/
+
+mkdir -p ~/.config/nvim/spell
+ln -sf ~/dotfiles/spell/* ~/.config/nvim/spell
+
 # Make the folder where snippets are actually stored
 mkdir -p ~/.config/nvim/UltiSnips
 # Copy any new snippets files to dotfiles repo
